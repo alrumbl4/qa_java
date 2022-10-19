6d3054b399a940f2bdcac1e0d8553fcd7c5ddb60
Alexey Rumyntsev <lean_back@mail.ru>
2022-10-19 11:10:17 +0300

    first commit

--- src/main/java/com/example/Animal.java
+++ src/main/java/com/example/Animal.java
@@ -2,8 +2,10 @@ package com.example;
 
 import java.util.List;
 
+//Создали класс "Животное"
 public class Animal {
 
+    //Создали метод getFood, который принимает на вход строковую переменную animalKind и возвращает список значений
     public List<String> getFood(String animalKind) throws Exception {
         if ("Травоядное".equals(animalKind)) {
             return List.of("Трава", "Различные растения");
